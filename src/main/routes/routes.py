4852c4b5c0d124d2299e44f21d70a3d68f60dcd1
samuel.batista <samuelsantxxi@gmail.com>
2022-04-26 16:35:31 -0300

    chore: compose

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -8,6 +8,7 @@ router_bp = Blueprint('any', __name__)
 @router_bp.route('/user', methods=["POST"])
 def hello_user():
     data = adpt(request, makeAddUserController().handle)
+    print(request.headers, dict(request.args))
     if(isinstance(data, dict)):
         print("Acertou mireravi!!")
     return jsonify(data.body), data.status_code
